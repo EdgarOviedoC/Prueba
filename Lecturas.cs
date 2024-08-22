9ea5ac44688dc39adf07bb75b9d348a4fe1ef75c
Edgar Oviedo <eoviedocuevas.2003@gmail.com>
2024-08-22 15:45:36 -0600

    Prueba

---
+++ Lecturas.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prueba
+{
+    public class Lecturas : IDisposable
+    {
+        StreamReader archivo;
+        StreamWriter log;
+        public Lecturas(){
+            archivo = new StreamReader("prueba.cpp");
+            log = new StreamWriter("prueba.log");
+        }
+
+        public Lecturas(string nombre){
+            archivo = new StreamReader(nombre);
+            log = new StreamWriter("prueba.log");
+        }
+
+        public void Dispose(){
+            Console.WriteLine("Destructor");
+            archivo.Close();
+            log.Close();
+        }
+    }
+}
